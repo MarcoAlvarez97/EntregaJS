eb9ccc2f50737292be1740eecc45c53c54178193
MarcoAlvarez <marco_alvarez@live.com.ar>
2023-11-18 09:23:22 -0300

    Ejercicios 3 y 4

--- main.js
+++ main.js
@@ -23,3 +23,25 @@ function compareNumbers(num1, num2) {
 }
 
 compareNumbers(5, 8);
+
+// 3  Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
+
+function multiplodecinco(numero) {
+  if (numero % 5 === 0) {
+    console.log(numero + " es multiplo de 5.");
+  } else {
+    console.log(numero + " no es multiplo de 5.");
+  }
+}
+
+multiplodecinco(7);
+
+// 4 Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
+
+function impresiondenumeros(numero) {
+  for (let i = 0; i <= numero; i++) {
+    console.log(i);
+  }
+}
+
+impresiondenumeros(10);
