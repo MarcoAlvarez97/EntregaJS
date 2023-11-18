e70eea2e3468cd18cf2d14490bf8bf805a5f9de2
MarcoAlvarez <marco_alvarez@live.com.ar>
2023-11-18 09:36:33 -0300

    Ejercicios 7 y 8, ya finalizado

--- main.js
+++ main.js
@@ -45,3 +45,51 @@ function impresiondenumeros(numero) {
 }
 
 impresiondenumeros(10);
+
+// 5 Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
+
+function imprimirpalabra(palabra, numero) {
+  for (let i = 0; i < numero; i++) {
+    console.log(palabra);
+  }
+}
+
+imprimirpalabra("JavaScript", 10);
+
+// 6 Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
+
+function printArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+
+// Ejemplo
+const miArray = [1, 2, 3, 4, 5];
+printArray(miArray);
+
+// 7 Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
+
+function printArraysinquintoElemento(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (i !== 4) {
+      console.log(array[i]);
+    }
+  }
+}
+
+// Ejemplo
+const miArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+printArraysinquintoElemento(miArray2);
+
+// 8 Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
+
+function multiplicarArrayPor(array, numero) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i] * numero);
+  }
+}
+
+// Ejemplo
+const miArray3 = [1, 2, 3, 4, 5];
+multiplicarArrayPor(miArray3, 10);
